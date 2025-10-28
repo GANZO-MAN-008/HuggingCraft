ce553187225d9920050920c726017af81c7c207a
GANZO-MAN-008 <154022325+GANZO-MAN-008@users.noreply.github.com>
2025-10-28 20:47:38 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,13 +2,14 @@
 
 ls
 
-./ngrok update
+ngrok update
 
-./ngrok config add-authtoken $NGROK_TOKEN
+ngrok config add-authtoken $NGROK_TOKEN
 
-./ngrok tcp 7860 > /dev/null &
+ngrok tcp 7860 > /dev/null &
 
 java -Xmx14336M -Xms14336M  -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Daikars.new.flags=true -Dusing.aikars.flags=https://mcflags.emc.gs -jar purpur.jar --nogui
+
 
 
 
